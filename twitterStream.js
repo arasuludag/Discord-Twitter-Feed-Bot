2fc8a9364cbab537a6fa61c7be05aec922d5a541
Aras Uludag <arasuludag@gmail.com>
2022-06-29 20:32:44 +0300

    Fix restarting

--- twitterStream.js
+++ twitterStream.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const { ETwitterStreamEvent, TwitterApi } = require("twitter-api-v2");
 
 const client = new TwitterApi({
@@ -9,13 +8,31 @@ const client = new TwitterApi({
 });
 
 async function twitterStream(discordClient) {
-  const newsChannel = await discordClient.channels.cache.find(
-    (channel) => channel.id === process.env.AVTNEWSCHANNELID
-  );
-
   const stream = await client.v1.stream.getStream("statuses/filter.json", {
     follow: process.env.TWITTERIDS.split(","),
   });
+
+  function sendMessage(message) {
+    discordClient.guilds.cache.forEach((guild) => {
+      const channel = guild.channels.cache.find(
+        (c) =>
+          c.type === "GUILD_TEXT" &&
+          guild.me.permissionsIn(c).has("SEND_MESSAGES") &&
+          c.name === process.env.NEWSCHANNELNAME
+      );
+      if (channel)
+        channel
+          .send(message)
+          .then(() => console.log(`Sent on ${guild.name} - ${channel.name}`))
+          .catch(console.error);
+      else
+        console.log(
+          "On guild " +
+            guild.name +
+            " I could not find a channel where I can type."
+        );
+    });
+  }
 
   // Emitted on Tweet
   stream.on(ETwitterStreamEvent.Data, async (tweet) => {
@@ -24,21 +41,18 @@ async function twitterStream(discordClient) {
       process.env.TWITTERIDS.split(",").some((id) => tweet.user.id_str === id)
     ) {
       if (tweet.retweeted_status) {
-        newsChannel.send(
+        sendMessage(
           `${tweet.user.screen_name} retweeted: https://twitter.com/${tweet.retweeted_status.user.screen_name}/status/${tweet.retweeted_status.id_str}`
         );
       } else {
-        newsChannel
-          .send(
-            `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-          )
-          .then((message) => {
-            if (tweet.quoted_status) {
-              message.reply(
-                `${tweet.user.name} quoted this: \n https://twitter.com/${tweet.quoted_status.user.screen_name}/status/${tweet.quoted_status.id_str}`
-              );
-            }
-          });
+        sendMessage(
+          `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+        );
+        if (tweet.quoted_status) {
+          sendMessage(
+            `${tweet.user.name} quoted this: \n https://twitter.com/${tweet.quoted_status.user.screen_name}/status/${tweet.quoted_status.id_str}`
+          );
+        }
       }
     }
   });
@@ -51,7 +65,8 @@ async function twitterStream(discordClient) {
     console.log("Twitter could not reconnect. ", number);
 
     // PM2 will restart Node when this happens.
-    throw new Error("Twitter could not reconnect. ", number);
+    console.log("Exiting Node.js");
+    process.exit(1);
   });
 
   stream.on(ETwitterStreamEvent.ConnectionClosed, async () => {
